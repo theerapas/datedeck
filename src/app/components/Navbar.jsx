66e78b24dc1811dc36925a207826d36fe48ea08b
Aluminium51 <noptanadon.p@gmail.com>
2025-03-11 13:53:12 +0700

    create some page folder

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='space-x-10'>
+        <a href="/login">Login</a>
+        <a href="/home">Home</a>
+        <a href="dashboard">Dashboard</a>
+        <a href="account">Account</a>
+    </div>
+
+  )
+}
+
+export default Navbar
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Navbar from "./components/Navbar";
 
 export default function Home() {
   return (
@@ -96,6 +97,7 @@ export default function Home() {
           Go to nextjs.org →
         </a>
       </footer>
+      <Navbar/>
     </div>
   );
 }
